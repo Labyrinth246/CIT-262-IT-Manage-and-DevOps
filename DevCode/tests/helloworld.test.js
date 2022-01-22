b2c0405d83e23de5acb3e652f5b081288724a111
Spencer Baker <spencer.baker77@gmail.com>
2022-01-22 16:26:57 -0700

    fresh start

--- DevCode/tests/helloworld.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, it } from "@jest/globals"
-import sayHello from '../utils/helloworld.mjs'
-
-it("Should say hello",()=>{
-    const hello = sayHello();
-
-    expect(hello).toBe('hello');
-});
